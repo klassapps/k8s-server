6b9bb0bb5180a3b5e2bb89e3cadc0313c6e962f0
Cesar Omar <cesar@klassapp.com>
2023-10-03 11:41:58 -0600

    fix k8s instalation

--- 2.install-k8s.sh
+++ 2.install-k8s.sh
@@ -4,9 +4,11 @@ sudo apt-get update
 
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
 
 sudo apt-get update
 
